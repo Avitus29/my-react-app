0310257a4e0ea59ec7b1c2f1c77f6c534a979b6a
Avitus Valence <avitusvalence6@gmail.com>
2025-03-27 22:13:14 +0300

    refactor Header component with improved styling and structure

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,9 @@
 const Header = () => {
   return (
-    <div>
-      <h1>DOCTOR RESERVATION SYSTEM</h1>
-    </div>
-  )
-}
+    <header style={{ textAlign: 'center', fontFamily: 'serif', padding: '20px', backgroundColor: '#f4f4f4' }}>
+      <h1>Doctor Reservation System</h1>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
